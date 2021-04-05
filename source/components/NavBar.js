535d7aeedb18d7c4ae182c0fac1ff42c01c40139
Garrett Cox <gcox5714@gmail.com>
2021-04-05 09:29:00 -0400

    bamhurger

--- source/components/NavBar.js
+++ source/components/NavBar.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Image} from 'react-native';
 import {useDimensions} from '@react-native-community/hooks';
 
 const NavBar = props => {
     const dimensions = useDimensions();
-    return (<View style={[styles.nav, {width: dimensions.window.width}]}></View>);
+    return (
+    <View style={[styles.nav, {width: dimensions.window.width}]}>
+        <Image source={require('../../assets/bamhurger.png')} />
+    </View>);
 }
 
 const styles = StyleSheet.create({
