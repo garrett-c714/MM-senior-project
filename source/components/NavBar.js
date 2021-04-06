0eb166ba76885250f4c54e39b360d873011b9094
Garrett Cox <gcox5714@gmail.com>
2021-04-06 09:39:33 -0400

    NavBar Button 🎈

--- source/components/NavBar.js
+++ source/components/NavBar.js
@@ -1,22 +1,42 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text, Image} from 'react-native';
+import {View, StyleSheet, Text, Image, TouchableHighlight} from 'react-native';
 import {useDimensions} from '@react-native-community/hooks';
 
 const NavBar = props => {
     const dimensions = useDimensions();
+    const startAlign = 'flex-start';
+    const startPath = require('../../assets/bamhurger.png');
+    const [requirePath, setRequirePath] = useState(startPath);
+    const [align, setAlign] = useState(startAlign);
     const {bgColor} = props;
+
+    const handlePress = () => {
+        align === startAlign ? setAlign('flex-end') : setAlign(startAlign);
+        requirePath === startPath ? setRequirePath(require('../../assets/x-icon.png')) : setRequirePath(startPath);
+    }
+
+    const internalStyles = {width: dimensions.window.width, 
+                            backgroundColor: bgColor,
+                            justifyContent: align}
+
     return (
-    <View style={[styles.nav, {width: dimensions.window.width, backgroundColor: bgColor}]}>
-        <Image style={styles.img} source={require('../../assets/bamhurger.png')} />
-    </View>);
+        <View style={[styles.nav, internalStyles]}>
+            <TouchableHighlight onPress={handlePress} > 
+                <Image style={styles.img} source={requirePath} />
+            </TouchableHighlight>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
     nav: {
-        //backgroundColor: 'blue',
+        flexDirection: 'row',
+        alignItems: 'center',
         height: 65
     },
     img: {
+        marginLeft: 15,
+        marginRight: 15,
         height: 60,
         width: 60
     }
