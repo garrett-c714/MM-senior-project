4a4bc7cea928313b7c452f494a92bbb80b1f85a0
Garrett Cox <gcox5714@gmail.com>
2021-04-07 09:43:04 -0400

    Working On Header 🍔

--- source/screens/Home.js
+++ source/screens/Home.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {SafeAreaView, View, StyleSheet, Button, Text} from 'react-native';
+import {SafeAreaView, View, StyleSheet, Button, Text, Image} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import NavBar from '../components/NavBar.js';
 
@@ -16,8 +16,18 @@ const Home = props => {
       <View>
         <NavBar bgColor={'dodgerblue'} />
       </View>
+      <View style={styles.headerBox}> 
+        <View style={styles.headerText}>
+            <Text style={styles.welcome}>Welcome, Garrett!</Text>
+            <Text>-- Wednesday, April 7th, 2021 --</Text>
+        </View>
+        <View style={styles.buttonBox}>
+          <Image style={styles.img} source={require('../../assets/bamhurger.png')} />
+          <Image style={styles.img} source={require('../../assets/bamhurger.png')} />
+        </View>
+      </View>
       <View style={styles.main}>
-      <Text>{textContent}</Text>
+        <Text>{textContent}</Text>
         <Button onPress={handlePress} title="Click Me!" />
       </View>
       <StatusBar style="auto" />
@@ -37,6 +47,38 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
       },
+      headerBox: {
+        //backgroundColor: 'purple',
+        flexDirection: 'row',
+        height: 200,
+        width: '100%',
+        //borderColor: 'purple',
+        //borderWidth: 4
+      },
+      headerText: {
+        borderWidth: 2,
+        borderColor: 'black',
+        height: '100%',
+        flexGrow: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      buttonBox: {
+        borderWidth: 2,
+        borderColor: 'green',
+        flexGrow: 3,
+        backgroundColor: 'black',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+      },
+      welcome: {
+        fontSize: 50,
+        fontFamily: 'Arial',
+      },
+      img: {
+        width: 10,
+        height: 10
+      }
 });
 
 export default Home;
