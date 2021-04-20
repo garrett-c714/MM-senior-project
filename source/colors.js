a2124be75d50241959bc02d7385261341c77f172
Garrett Cox <gcox5714@gmail.com>
2021-04-20 09:42:18 -0400

    Finalizing Weather Widget 🌂

--- source/colors.js
+++ source/colors.js
@@ -3,7 +3,8 @@ const colors = {
     peach: '#F4BB57',
     gray: '#C4C4C4',
     charcoal: '#1B1A1A',
-    red: '#F84444'
+    red: '#F84444',
+    blue: '#26C4DB'
 };
 
 export default colors;
--- source/components/Modal.js
+++ source/components/Modal.js
@@ -1,21 +1,61 @@
 import React from 'react';
 import {View, StyleSheet, Image, Text, TouchableHighlight} from 'react-native';
+import { useDimensions } from '@react-native-community/hooks';
 import { useFonts } from 'expo-font';
 import Colors from '../colors.js';
 
 const Modal = props => {
+    const dimensions = useDimensions();
     const {barColor, XClick} = props;
+    
     return (
-        <View style={styles.container}>
-            <View style={[styles.bar, {backgroundColor: barColor}]}>
-                <TouchableHighlight><Image style={styles.img} source={require('../../assets/x-icon.png')} /></TouchableHighlight>
+        <View style={[styles.biggerContainer, {width: dimensions.window.width, height: dimensions.window.height}]}>
+            <View style={styles.container}>
+                <View style={[styles.bar, {backgroundColor: barColor}]}>
+                    <TouchableHighlight><Image style={styles.Ximg} source={require('../../assets/x-icon.png')} /></TouchableHighlight>
+                </View>
+                <View style={styles.head}>
+                    <View style={styles.imgBox}>
+                        <Image style={styles.img} source={require('../../assets/sun-icon-orange.png')} />
+                        <Text style={styles.captions}>Sunny</Text>
+                    </View>
+                    <Text style={styles.bigText}>69°</Text> 
+                </View>
+                <View style={styles.barWrapper}><View style={styles.rectangle}></View></View>
+                <View style={styles.body}>
+                    <View style={styles.column}>
+                        <Image style={styles.bodyImg} source={require('../../assets/thermo-icon.png')} />
+                        <Image style={styles.bodyImg} source={require('../../assets/rain-icon.png')} />
+                        <Image style={styles.bodyImg} source={require('../../assets/wind-icon.png')} />
+                    </View>
+                    <View style={styles.column}>
+                        <View style={styles.dot}></View>
+                        <View style={styles.dot}></View>
+                        <View style={styles.dot}></View>
+                    </View>
+                    <View style={styles.column}>
+                        <Text style={styles.bodyText}>
+                            <Text style={{color: Colors.red}}>77</Text>
+                            <Text>  /  </Text>
+                            <Text style={{color: Colors.blue}}>55</Text>
+                        </Text>
+                        <Text style={styles.bodyText}>34%</Text>
+                        <Text style={styles.bodyText}>4mph</Text>
+                    </View>
+                </View>
             </View>
-            <View style={styles.main}></View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    biggerContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     container: {
         borderRadius: 25,
         width: 350,
@@ -31,13 +71,67 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 25,
         height: 70
     },
-    img: {
+    Ximg: {
         height: 50,
         width: 50,
         margin: 15
     },
-    main: {
-
+    img: {
+        width: 130,
+        height: 130
+    },
+    head: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        margin: 20
+    },
+    imgBox: {
+        alignItems: 'center'
+    },
+    captions: {
+        color: Colors.gray,
+        margin: 5
+    },
+    bigText: {
+        fontSize: 90,
+        color: Colors.gray
+    },
+    rectangle: {
+        backgroundColor: Colors.gray,
+        height: 8,
+        width: "75%",
+        borderRadius: 10,
+        margin: 10
+    },
+    barWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    body: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    column: {
+        height: "100%",
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: 80
+    },
+    bodyImg: {
+        width: 50,
+        height: 50
+    },
+    dot: {
+        width: 10,
+        height: 10,
+        borderRadius: "50%",
+        backgroundColor: Colors.gray
+    },
+    bodyText: {
+        fontSize: 20,
+        color: Colors.gray
     }
 });
 
