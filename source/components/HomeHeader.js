7216649bc017f916b6ec76f81cefe207e33a4316
Garrett Cox <gcox5714@gmail.com>
2021-04-22 08:42:38 -0400

    Date Works Now 🎶

--- source/components/HomeHeader.js
+++ source/components/HomeHeader.js
@@ -1,14 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Text, Image, ImageBackground} from 'react-native';
+
+const getDate = () => {
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  const date = new Date();
+  const number = date.getDate();
+  let suffix = "";
+
+
+  switch(number.toString().substring(number.toString().length - 1)) {
+      case "1":
+          suffix = "st";
+          break;
+      case "2":
+          suffix = "nd";
+          break;
+      case "3":
+          suffix = "rd";
+          break;
+      default: 
+          suffix = "th"
+          break;
+  }
+
+  return(`${days[date.getDay()]}, ${months[date.getMonth()]} ${number}${suffix}, ${date.getFullYear()}`);
+}
 
 const HomeHeader = props => {
   const image = require('../../assets/header-background.jpg');
+  const [date, setDate] = useState("");
 
-    return (
+  useEffect(() => {
+    setDate(getDate());
+  }, [])
+
+  return (
       <View style={styles.headerBox}> 
         <ImageBackground source={ image } style={styles.headerText}>
             <Text style={[styles.welcome, styles.text]}>Welcome, Garrett!</Text>
-            <Text style={[styles.text, styles.raleway]}>-- Wednesday, April 7th, 2021 --</Text>
+            <Text style={[styles.text, styles.raleway]}>-- {date} --</Text>
         </ImageBackground>
       </View>
     );
