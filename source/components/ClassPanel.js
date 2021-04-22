badf6b2d4cd23574d867317b6488ef2fb7d92378
Garrett Cox <gcox5714@gmail.com>
2021-04-22 09:30:04 -0400

    Started Classes / Context 🤑

---
+++ source/components/ClassPanel.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, Text, Image, TouchableHighlight} from 'react-native';
+
+import Colors from '../colors.js';
+
+const ClassPanel = props => {
+    return (null);
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default ClassPanel;
---
+++ source/context/ClassContext.js
@@ -0,0 +1,38 @@
+import React, { createContext, useState, useEffect } from 'react';
+import Colors from '../colors.js';
+
+import Colors from '../colors.js';
+
+const ClassContext = createContext();
+
+const ClassProvider = props => {
+    const [classes, setClasses] = useState([{name: 'Calculus', key: 0}, {name: 'Physics', key: 1}]);
+    const [number, setNumber] = useState(classes.length);
+
+    useEffect(() => {
+        setNumber(number+1);
+    }, [classes]);
+    
+    const addClass = newClass => {
+        setClasses([...classes, newClass]);
+    }
+    const removeClass = key => {
+        const newClasses = classes.filter(c => c.key != key);
+        setClasses(newClasses);
+    }
+
+    const value = {
+        classes: classes,
+        addClass: addClass,
+        removeClass: removeClass,
+        number: number
+    }
+
+    return (
+        <ClassContext.Provider value={value}>
+            {props.children}
+        </ClassContext.Provider>
+    )
+}
+
+export { ClassContext, ClassProvider };
