2796fec25ec2d930801bf4392f7f564de5a89540
Garrett Cox <gcox5714@gmail.com>
2021-04-22 09:06:48 -0400

    Gesture Recognizer? 🪐

--- source/components/HomeScroll.js
+++ source/components/HomeScroll.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
 import {View, StyleSheet, Image, Text} from 'react-native';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 
 import {ListContext} from '../context/ListContext.js';
 import NumberBox from "./NumberBox";
@@ -8,6 +9,12 @@ import Colors from '../colors.js';
 
 const HomeScroll = props => {
     const context = useContext(ListContext);
+    const [testText, setTestText] = useState("");
+    
+    useEffect(() => {
+        console.log(testText);
+    }, [testText]);
+    
     let alertColor = Colors.brightGreen;
 
     if (context.numThings >= 4 ) {
@@ -18,7 +25,33 @@ const HomeScroll = props => {
         alertColor = Colors.yellow;
     }
 
+    function onSwipeUp(gestureState) {
+        setTestText("swiped up!");
+    }
+    function onSwipeDown(gestureState) {
+        setTestText('swiped down!');
+    }
+    function onSwipeLeft(gestureState) {
+        setTestText('swiped left!');
+    }
+    function onSwipeRight(gestureState) {
+        setTestText('swiped right!');
+    }
+
+    const config = {
+        velocityThreshold: 0.3,
+        directionalOffsetThreshold: 80
+    }
+
     return (
+        <GestureRecognizer
+            onSwipeUp={(state) => {onSwipeUp(state)}}
+            onSwipeDown={(state) => {onSwipeDown(state)}}
+            onSwipeLeft={(state) => {onSwipeLeft(state)}}
+            onSwipeRight={(state) => {onSwipeRight(state)}}
+            config={config}
+            style={{flex: 1, width: "100%"}}
+        >
         <View style={styles.main}>
             <View style={styles.bar}></View>
             <View style={styles.textContainer}>
@@ -29,6 +62,7 @@ const HomeScroll = props => {
                 <Text style={styles.caption}>Swipe up to see your daily breakdown...</Text>
             </View>
         </View>
+        </GestureRecognizer>
     );
 }
 
