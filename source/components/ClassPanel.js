b8ed82c4c51c8884cc8a44cf2eec334db967dcc6
Garrett Cox <gcox5714@gmail.com>
2021-04-29 09:41:49 -0400

    Working on Classes 🛸

--- source/components/ClassPanel.js
+++ source/components/ClassPanel.js
@@ -4,16 +4,46 @@ import {View, StyleSheet, Text, Image, TouchableHighlight} from 'react-native';
 import Colors from '../colors.js';
 
 const ClassPanel = props => {
+    const {info} = props;
+
     return (
         <View style={styles.container}>
+            <View style={styles.left}>
+                <Text style={styles.name}>{info.name}</Text>
+            </View>
+            <View style={styles.right}>
 
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        
+        backgroundColor: Colors.peach,
+        width: '90%',
+        height: 200,
+        margin: 15,
+        borderRadius: 25,
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    left: {
+        flex: 1,
+        flexGrow: 2,
+        borderTopLeftRadius: 25,
+        borderBottomLeftRadius: 25,
+        padding: 10
+    },
+    right: {
+        flex: 1,
+        flexGrow: 1,
+        borderTopRightRadius: 25,
+        borderBottomRightRadius: 25
+    },
+    name: {
+        color: 'white',
+        fontSize: 40
     }
 });
 
--- source/screens/Classes.js
+++ source/screens/Classes.js
@@ -2,7 +2,8 @@ import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Image, Text, SafeAreaView, Button, ScrollView} from 'react-native';
 import NavBar from '../components/NavBar';
 import Nav from '../components/Nav.js';
-import WeatherModal from '../components/WeatherModal.js';
+import ClassPanel from '../components/ClassPanel.js';
+
 import Colors from '../colors.js';
 
 const Classes = props => {
@@ -12,8 +13,6 @@ const Classes = props => {
     const [requirePath, setRequirePath] = useState(startPath);
     const [align, setAlign] = useState(startAlign);
 
-    const [rory, setRory] = useState([]);
-    const [roryKey, setRoryKey] = useState(0);
 
     useEffect(() => {
         if (navOpen === true) {
@@ -29,39 +28,23 @@ const Classes = props => {
       setNavOpen(!navOpen);
     }
 
-    const roryPress= () => {
-        const num = Number.parseInt(Math.random()*3+1, 10);
-        let imgSource;
-        switch(num) {
-            case 1:
-                imgSource = require('../../assets/rory-1.jpg');
-                break;
-            case 2:
-                imgSource = require('../../assets/rory-2.jpg');
-                break;
-            default:
-                imgSource = require('../../assets/rory-3.jpg');
-        }
-        const newRory = {
-            source: imgSource,
-            key: roryKey
-        };
-        setRory([...rory, newRory]);
-        setRoryKey(roryKey+1);
+    /*Temp*/
+    const info = {
+        name: 'AP Calculus BC',
+        room: '300'
     }
+
+    /* ---- */
 
     return (
     <SafeAreaView style={styles.container}>
         {navOpen && <Nav />}
         <NavBar bgColor={Colors.mintGreen} handlePress={handlePress} requirePath={requirePath} align={align} />
         <View style={styles.main}>
-            <View style={styles.temp}>
-                <Text style={styles.tempText}>Classes are a feature currently in the works...</Text>
-                <Button onPress={roryPress} style={styles.button} title='Click for Surpise :)' />
+            <ClassPanel info={info} />
+            <View style={styles.plusView}>
+                <Image style={styles.plusImg} source={require('../../assets/add-button.png')} />
             </View>
-            <ScrollView>
-                {rory.map(r => <Image key={r.key} source={r.source} style={styles.image} />)}
-            </ScrollView>
         </View>
     </SafeAreaView>
     );
@@ -74,24 +57,21 @@ const styles = StyleSheet.create({
     },
     main: {
         flex: 1,
-        backgroundColor: 'purple',
+        backgroundColor: 'white',
         justifyContent: 'flex-start',
         alignItems: 'center'
     },
-    temp: {
-        backgroundColor: 'white',
-        justifyContent:'space-evenly',
+    plusView: {
+        flexDirection: 'row',
+        justifyContent: 'center',
         alignItems: 'center',
-        height: 200,
-        width: 400
+        width: '100%',
+        height: 150,
+        marginTop: 'auto',
     },
-    tempText: {
-        fontSize: 20,
-        fontWeight: '600'
-    },
-    image: {
-        width: 300,
-        height: 150
+    plusImg: {
+        width: 100,
+        height: 100
     }
 });
 
